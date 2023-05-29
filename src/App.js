d80c81ca13a010321fdc23e69065cd5003eaa62b
Phuc <phucbopbnation@gmail.com>
2023-05-29 23:33:28 +0700

    fix style

--- src/App.js
+++ src/App.js
@@ -26,49 +26,54 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Tìm kiếm</h1>
+        <h1>Tìm kiếm quán cà phê</h1>
         <button className="search-button" onClick={handleSearchClick}>
           <RiSearchLine />
-          <span>検索</span>
+          <span>Tìm kiếm</span>
         </button>
       </header>
 
       {isPopupOpen && (
-        <div id="search-popup">
-          <h2>検索</h2>
-          <button className="close-button" onClick={handlePopupClose}>
-            <RiCloseLine />
-          </button>
-          <form onSubmit={handleSearchSubmit}>
-            <label htmlFor="cafe-name">Tên quán cà phê:</label>
-            <input type="text" id="cafe-name" value={cafeName} onChange={(e) => setCafeName(e.target.value)} />
+        <div className="search-popup">
+          <div className="popup-content">
+            <h2>Tìm kiếm</h2>
+            <button className="close-button" onClick={handlePopupClose}>
+              <RiCloseLine />
+            </button>
+            <form onSubmit={handleSearchSubmit}>
+              <div className="form-group">
+                <label htmlFor="cafe-name">Tên quán cà phê:</label>
+                <input type="text" id="cafe-name" value={cafeName} onChange={(e) => setCafeName(e.target.value)}/>
+              </div>
 
-            <label htmlFor="area">Khu vực:</label>
-            <select id="area" value={area} onChange={(e) => setArea(e.target.value)}>
-              <option value="all">Tất cả</option>
-              <option value="A">Hai Ba Trung</option>
-              <option value="B">Dong Da</option>
-              <option value="C">Hoan Kiem</option>
-            </select>
+              <div className="form-group">
+                <label htmlFor="area">Khu vực:</label>
+                <select id="area" value={area} onChange={(e) => setArea(e.target.value)}>
+                  <option value="all">Tất cả</option>
+                  <option value="A">Hai Bà Trưng</option>
+                  <option value="B">Đống Đa</option>
+                  <option value="C">Hoàn Kiếm</option>
+                </select>
+              </div>
 
-            <div className="input-container">
-            <label htmlFor="has-ac">Có máy điều hòa:</label>
-            
-            <label htmlFor="status">Trạng thái:</label>
-            </div>
+              <div className="form-group">
+                <label htmlFor="has-ac">Có máy điều hòa:</label>
+                <input type="checkbox" className="checkbox-1" id="has-ac" checked={hasAC} onChange={(e) => setHasAC(e.target.checked)} />
+              </div>
 
-            <div className="input-container">
-            <input type="checkbox" id="has-ac" checked={hasAC} onChange={(e) => setHasAC(e.target.checked)} />
-            
-            <select id="status" value={status} onChange={(e) => setStatus(e.target.value)}>
-              <option value="all">Tất cả</option>
-              <option value="open">Đang mở cửa</option>
-            </select>
-            </div>
+              <div className="form-group">
+                <label htmlFor="status">Trạng thái:</label>
+                <select id="status" value={status} onChange={(e) => setStatus(e.target.value)}>
+                  <option value="all">Tất cả</option>
+                  <option value="open">Đang mở cửa</option>
+                </select>
+              </div>
 
-            <button type="submit">Tìm kiếm</button>
-            <button type="button" onClick={handlePopupClose}>Đóng</button>
-          </form>
+              <div className="button-group">
+                <button type="submit">Tìm kiếm</button>
+              </div>
+            </form>
+          </div>
         </div>
       )}
     </div>
