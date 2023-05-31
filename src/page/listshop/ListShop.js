009eb4d430547ebfe6c511ce805a363f489700ce
MinhDangDinh <ddm20012001@gmail.com>
2023-06-01 04:05:52 +0700

    minh

---
+++ src/page/listshop/ListShop.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './listshop.css';
+
+
+export default function Gallery() {
+  return (
+    <>
+      <div className="gallery">
+        <a target="_blank" href="https://haycafe.vn/wp-content/uploads/2022/03/anh-quan-cafe-1.jpg">
+          <img src="https://haycafe.vn/wp-content/uploads/2022/03/anh-quan-cafe-1.jpg" alt="Cinque Terre" width="600" height="400" />
+        </a>
+        
+        <ul className="a">
+          <li>Coffee</li>
+          <li>*****</li>
+          <li>155 Cầu Giấy</li>
+          <li>8:00-22:00</li>
+        </ul>
+      </div>
+
+      <div className="gallery">
+      <a target="_blank" href="https://haycafe.vn/wp-content/uploads/2022/03/anh-quan-cafe-1.jpg">
+          <img src="https://haycafe.vn/wp-content/uploads/2022/03/anh-quan-cafe-1.jpg" alt="Cinque Terre" width="600" height="400" />
+        </a>
+        <ul className="a">
+          <li>Coffee</li>
+          <li>*****</li>
+          <li>155 Cầu Giấy</li>
+          <li>8:00-22:00</li>
+        </ul>
+      </div>
+
+      <div className="gallery">
+      <a target="_blank" href="https://haycafe.vn/wp-content/uploads/2022/03/anh-quan-cafe-1.jpg">
+          <img src="https://haycafe.vn/wp-content/uploads/2022/03/anh-quan-cafe-1.jpg" alt="Cinque Terre" width="600" height="400" />
+        </a>
+        <ul className="a">
+          <li>Coffee</li>
+          <li>*****</li>
+          <li>155 Cầu Giấy</li>
+          <li>8:00-22:00</li>
+        </ul>
+      </div>
+
+      <div className="gallery">
+      <a target="_blank" href="https://haycafe.vn/wp-content/uploads/2022/03/anh-quan-cafe-1.jpg">
+          <img src="https://haycafe.vn/wp-content/uploads/2022/03/anh-quan-cafe-1.jpg" alt="Cinque Terre" width="600" height="400" />
+        </a>
+        <ul className="a">
+          <li>Coffee</li>
+          <li>*****</li>
+          <li>155 Cầu Giấy</li>
+          <li>8:00-22:00</li>
+        </ul>
+      </div>
+    </>
+  );
+  }
