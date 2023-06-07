9c6f4352af61bdb4559e747b43e57caf136f5c00
June24-01 <huyquangtr01@gmail.com>
2023-06-07 10:12:03 +0700

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Header from './components/header/Header';
 import './App.css';
+
 import InforShop from './pages/InforShop/InforShop';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <InforShop />
+      {/* <InforShop /> */}
+      <Home />
+      {/* < Login /> */}
     </div>
   );
 }
