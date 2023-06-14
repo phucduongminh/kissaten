90441ce07e4e9ccb74102b567cad8929955a4fe7
June24-01 <huyquangtr01@gmail.com>
2023-06-14 13:03:37 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import Header from './components/header/Header';
 import './App.css';
-
 import InforShop from './pages/InforShop/InforShop';
-import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
-
+import Home from './pages/Home/Home';
+import Bookmark from './pages/Bookmark/Bookmark';
+import Approve from './pages/Approve/Approve';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path ='/' element={<Home />} />
+        <Route path ='/inforshop/:id' element={<InforShop />} />
+        <Route path ='/bookmark' element={<Bookmark/>} />
+        <Route path ='/approve' element= {<Approve />} />
+      </Routes>
+      {/* <Home /> */}
+      {/* <Login /> */}
       {/* <InforShop /> */}
-      <Home />
-      {/* < Login /> */}
+      {/* <Bookmark /> */}
+      {/* <Approve /> */}
     </div>
   );
 }
-
 
 export default App;
