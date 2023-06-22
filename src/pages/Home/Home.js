8e3ac39e839df29e5e25ab1910df6bd68f23a0cc
Phuc <phucbopbnation@gmail.com>
2023-06-22 12:21:28 +0700

    pulldown home done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,32 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import './home.css';
-import { useNavigate } from 'react-router-dom';
-import Bookmark from '../../components/Bookmark/bookmark';
-import axios from 'axios';
-import { useSelector, useDispatch } from 'react-redux';
-import Showrating from '../../components/Rating/showrating';
-import { updateBookmarkedItemIds } from '../Login/loginActions';
+import React, { useState, useEffect } from "react";
+import "./home.css";
+import { useNavigate } from "react-router-dom";
+import Bookmark from "../../components/Bookmark/bookmark";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import Showrating from "../../components/Rating/showrating";
+import { updateBookmarkedItemIds } from "../Login/loginActions";
 
 const Home = () => {
   const user = useSelector((state) => state.login.user);
-  const bookmarkedItemIds = useSelector((state) => state.login.bookmarkedItemIds);
+  const bookmarkedItemIds = useSelector(
+    (state) => state.login.bookmarkedItemIds
+  );
   const dispatch = useDispatch();
 
   const [shop, setShop] = useState([]);
+  const [pulldown, setPulldown] = useState('All');
 
   useEffect(() => {
     const axiosGetshop = async () => {
-      const response = await axios.post(`https://localhost:7263/api/CoffeeShop/SortCoffeeShopSortBy/All`);
+      const response = await axios.post(
+        `https://localhost:7263/api/CoffeeShop/SortCoffeeShopSortBy/${pulldown}`
+      );
       const data = await response.data;
       setShop(data);
     };
     axiosGetshop();
-  }, []);
+  }, [pulldown]);
+
+  const handlePulldownChange = (event) => {
+    const selectedValue = event.target.value;
+    setPulldown(selectedValue);
+  };
 
   useEffect(() => {
     const axiosGetBookmarkedItemIds = async () => {
       if (user) {
-        const response = await axios.post(`https://localhost:7263/api/BookMark/${user.uid}/getListBookMark`);
+        const response = await axios.post(
+          `https://localhost:7263/api/BookMark/${user.uid}/getListBookMark`
+        );
         const data = await response.data;
         const bookmarkedItemIds = data.map((item) => item.id);
         dispatch(updateBookmarkedItemIds(bookmarkedItemIds));
@@ -37,15 +49,38 @@ const Home = () => {
 
   const handleBookmarkClick = async (itemId) => {
     if (bookmarkedItemIds.includes(itemId)) {
-      await axios.delete(`https://localhost:7263/api/BookMark/DeleteBookMarkById/${user.uid}/${itemId}`);
-      dispatch(updateBookmarkedItemIds(bookmarkedItemIds.filter((id) => id !== itemId)));
+      await axios.delete(
+        `https://localhost:7263/api/BookMark/DeleteBookMarkById/${user.uid}/${itemId}`
+      );
+      dispatch(
+        updateBookmarkedItemIds(bookmarkedItemIds.filter((id) => id !== itemId))
+      );
     } else {
-      await axios.post(`https://localhost:7263/api/BookMark/${user.uid}/AddBookMark/${itemId}`);
+      await axios.post(
+        `https://localhost:7263/api/BookMark/${user.uid}/AddBookMark/${itemId}`
+      );
       dispatch(updateBookmarkedItemIds([...bookmarkedItemIds, itemId]));
     }
   };
 
   const navigate = useNavigate();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const shopsPerPage = 6;
+
+  // Tính chỉ số các shop trong danh sách cần hiển thị trên trang hiện tại
+  const indexOfLastShop = currentPage * shopsPerPage;
+  const indexOfFirstShop = indexOfLastShop - shopsPerPage;
+  const currentShops = shop.slice(indexOfFirstShop, indexOfLastShop);
+
+  // Tạo mảng phân trang dựa trên số lượng coffee shop và số lượng shop trên mỗi trang
+  const pageNumbers = Array.from(
+    { length: Math.ceil(shop.length / shopsPerPage) },
+    (_, index) => index + 1
+  );
+
+  // Hàm chuyển trang
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   if (!shop) {
     return <div>Loading...</div>;
@@ -57,16 +92,16 @@ const Home = () => {
         <div className="home-heading">
           <h2 className="title">ホームページ</h2>
           <div className="filter">
-            <label htmlFor="">Sort by</label>
-            <select name="" id="">
-              <option value="">全部</option>
-              <option value="1">平均評価</option>
-              <option value="2">人気がある</option>
+            <label>Sort by</label>
+            <select name="" id="" select onChange={handlePulldownChange}>
+              <option value="All">全部</option>
+              <option value="Rating">平均評価 ↑</option>
+              <option value="RatingDown">平均評価 ↓</option>
             </select>
           </div>
         </div>
         <div className="home-list">
-          {shop.map((item, index) => {
+          {currentShops.map((item, index) => {
             return (
               <div
                 className="home-item"
@@ -102,21 +137,21 @@ const Home = () => {
           })}
         </div>
         <div className="home-pagination">
-          <a className="page" href="!">
+          <div className="page">
             <i className="fa-solid fa-chevron-left"></i>
-          </a>
-          <a className="page" href="!">
-            1
-          </a>
-          <a className="page" href="!">
-            2
-          </a>
-          <a className="page" href="!">
-            3
-          </a>
-          <a className="page" href="!">
+          </div>
+          {pageNumbers.map((number) => (
+            <div
+              className={`page${number === currentPage ? " active" : ""}`}
+              key={number}
+              onClick={() => paginate(number)}
+            >
+              {number}
+            </div>
+          ))}
+          <div className="page">
             <i className="fa-solid fa-chevron-right"></i>
-          </a>
+          </div>
         </div>
       </div>
     </section>
